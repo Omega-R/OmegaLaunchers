c5e8e39f4d73667166408883329c3539849e856a
Anton Knyazev <anton.knyazev@omega-r.com>
2019-08-07 13:09:24 +0300

    Downgrade minVersionApi to 14

--- omegalauncherslib/src/main/java/com/omegar/libs/omegalaunchers/ActivityLauncher.kt
+++ omegalauncherslib/src/main/java/com/omegar/libs/omegalaunchers/ActivityLauncher.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.AndroidRuntimeException
@@ -86,7 +87,11 @@ class ActivityLauncher(
     }
 
     private fun Activity.compatStartActivityForResult(intent: Intent, requestCode: Int, option: Bundle? = null) {
-        startActivityForResult(intent, requestCode, option)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            startActivityForResult(intent, requestCode, option)
+        } else {
+            startActivityForResult(intent, requestCode)
+        }
     }
 
     fun getPendingIntent(
